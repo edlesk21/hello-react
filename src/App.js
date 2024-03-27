b70732e1be01f82396ce10b7b9c9a87517b5da9c
Ej Leskauskas <143560945+edlesk21@users.noreply.github.com>
2024-03-27 17:48:15 +0000

    Finsihed lecture

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
 import logo from './spartan.svg';
 import './App.css';
 
+const magazines = [
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: 'https://upload.wikimedia.org/wikipedia/en/4/48/Hemingway_farewell.png',
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      {/* Components can't return multiple JSX tags unless they are wrapped in a parent element */}
+      {/* This is a JSX comment */}
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      {/* This a conditional that checks if an image exists before displaying it */}
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+    </div>
+  );
+}
+
 function MagicButton(){
   return(
     <div> 
@@ -14,21 +66,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <div> 
-            <p> I hope this works, lets go Spartans!</p>
-          </div>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with E.J. and Sparty!
-        </a>
+        <ZineRack />
+        <Bookshelf />
         <MagicButton />
       </header>
     </div>
